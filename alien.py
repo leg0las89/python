af6cf8d061be442a9a00eddd37c2ffda5b6e4cf1
Viktor Kozinskiy <vkozinskiy@ipsoft.com>
2019-09-25 22:03:41 -0400

    new

--- alien.py
+++ alien.py
@@ -1,6 +1,12 @@
 aliens=[]
 
 for i in range(30):
-    alien={"color":"green","speed":"slow","id":alien}
+    alien={"color":"green","speed":"slow","id":i}
     aliens.append(alien)
-print(aliens)
+n=0
+for f in aliens[:10]:
+    if f["id"] % 2:
+        f["color"]="yelow"
+        f["speed"]="medium"
+    print(f"\n{aliens[n]}")
+    n+=1
